a4a0b31cd9e2775c32bf75482ae154d4afc48af6
Mariana0317 <mariana.granara@gmail.com>
2021-09-09 18:04:53 -0300

    first part backend

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+//midleware
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+//conexion con mongodb wl link de mongo
+const CONNECTION_URL =
+  "mongodb+srv://NinaNegraRomeo397411:NinaNegraRomeo397411@socialmedia.5gvfl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;//no me funciona en .env dotenv
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
